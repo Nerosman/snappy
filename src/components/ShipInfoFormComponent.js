0acfb5982811704afafeefaf1acede4252656233
nerosman <nerosman2601@gmail.com>
2018-11-07 00:32:17 +0200

    added possibility to save all inputs to state

--- src/components/ShipInfoFormComponent.js
+++ src/components/ShipInfoFormComponent.js
@@ -1,64 +1,97 @@
 import React from "react";
 import {InputComponent} from "./reusableComponents/InputComponent";
-import {Form} from "reactstrap";
+import {Button, Form} from "reactstrap";
 
-export const ShipInfoFormComponent = () => {
-    return <Form>
-        <InputComponent
-            id="first-name"
-            label="First name"
-            type="text"
-            onValid={true}
-            placeholder="John"
-        />
-        <InputComponent
-            id="second-name"
-            label="Second name"
-            type={"text"}
-            onValid={true}
-            placeholder="Whiskey"
-        />
-        <InputComponent
-            id="address"
-            label="Address"
-            type={"text"}
-            onValid={true}
-            placeholder="1234 Main St"
-        />
-        <InputComponent
-            id="city"
-            label="City"
-            type="text"
-            onValid={true}
-        />
-        <InputComponent
-            id="state"
-            label="State"
-            type="text"
-            onValid={true}
-        />
-        <InputComponent
-            id="zip"
-            label="Zip"
-            type="text"
-            onValid={true}
-        />
-        <InputComponent
-            id="email"
-            label="Email"
-            type={"email"}
-            onValid={true}
-        />
-        <InputComponent
-            id="phone"
-            label="Phone"
-            onValid={true}
-        />
-        <InputComponent
-            id="special-notes"
-            label="Special notes"
-            type={"textarea"}
-            onValid={true}
-        />
-    </Form>
-};
+export class ShipInfoFormComponent extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+
+    inputValueToState(e) {
+        this.setState({[e.target.name]: e.target.value})
+    }
+
+    submitForm() {
+        console.log(this.state)
+    }
+
+    render() {
+        return <Form>
+            <InputComponent
+                id="first-name"
+                name="firstName"
+                label="First name"
+                type="text"
+                onChange={this.inputValueToState.bind(this)}
+                onValid={true}
+                placeholder="John"
+            />
+            <InputComponent
+                id="second-name"
+                name="secondValue"
+                label="Second name"
+                type={"text"}
+                onChange={this.inputValueToState.bind(this)}
+                onValid={true}
+                placeholder="Whiskey"
+            />
+            <InputComponent
+                id="address"
+                name="address"
+                label="Address"
+                type={"text"}
+                onChange={this.inputValueToState.bind(this)}
+                onValid={true}
+                placeholder="1234 Main St"
+            />
+            <InputComponent
+                id="city"
+                name="city"
+                label="City"
+                type="text"
+                onChange={this.inputValueToState.bind(this)}
+                onValid={true}
+            />
+            <InputComponent
+                id="state"
+                name="state"
+                label="State"
+                type="text"
+                onChange={this.inputValueToState.bind(this)}
+                onValid={true}
+            />
+            <InputComponent
+                id="zip"
+                name="zip"
+                label="Zip"
+                type="text"
+                onChange={this.inputValueToState.bind(this)}
+                onValid={true}
+            />
+            <InputComponent
+                id="email"
+                name="email"
+                label="Email"
+                type={"email"}
+                onChange={this.inputValueToState.bind(this)}
+                onValid={true}
+            />
+            <InputComponent
+                id="phone"
+                name="phone"
+                label="Phone"
+                onChange={this.inputValueToState.bind(this)}
+                onValid={true}
+            />
+            <InputComponent
+                id="special-notes"
+                name="specialNotes"
+                label="Special notes"
+                type={"textarea"}
+                onChange={this.inputValueToState.bind(this)}
+                onValid={true}
+            />
+            <Button onClick={this.submitForm.bind(this)}>Submit</Button>
+        </Form>
+    }
+}
