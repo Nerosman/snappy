64e7bafb6779d0cd682a637708a633a9a8548072
nerosman <nerosman2601@gmail.com>
2018-11-08 18:20:41 +0200

    changes

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const port = process.env.PORT || 6000;
+const port = process.env.PORT || 5000;
 const mongoose = require('mongoose');
 const Task = require('./api/models/orderListModel');
 const bodyParser = require('body-parser');
